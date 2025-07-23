74c66084ececdaa76117e48fb7e72de900e65988
Anukriti-j <160719489+Anukriti-j@users.noreply.github.com>
2025-07-23 12:15:25 +0530

    Create ToDo3.swift

---
+++ Week_1/ToDo3.swift
@@ -0,0 +1,7 @@
+// TODO 3: Create empty collections using initializer syntax
+
+	var array2 = Array<String> ()  // Empty Array
+	var dict2 = [String:Int] ()          // Empty Dictionary
+	var set = Set<Int>()
+
+
