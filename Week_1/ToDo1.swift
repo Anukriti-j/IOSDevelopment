3a080c4fc5821a7ead579ab3b42421b4489fcf38
Anukriti-j <160719489+Anukriti-j@users.noreply.github.com>
2025-07-23 12:12:02 +0530

    To-Do 1 done

---
+++ Week_1/ToDo1.swift
@@ -0,0 +1,12 @@
+//Create variables and constants
+
+//Create a variable for your age and a constant for your name
+
+var  age: Int = 22
+let  name: String = "Anukriti"
+
+//Use string interpolation to create a greeting message
+
+print(“Hello! \(name) Welcome to Coditas.”)
+
+
