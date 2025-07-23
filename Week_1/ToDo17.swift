8980de931b1282c4723214b64edc18420330952f
Anukriti-j <160719489+Anukriti-j@users.noreply.github.com>
2025-07-23 13:41:16 +0530

    Create ToDo17.swift

---
+++ Week_1/ToDo17.swift
@@ -0,0 +1,15 @@
+enum NetworkError: Error{
+    case internetIssue
+    case invalidUrl
+}
+ func dataRequest(_ url: String) throws {
+     if url == "" {
+         throw NetworkError.internetIssue
+     }
+ }
+ do{
+     try dataRequest("")
+     print("valid request")
+ }catch{
+     print("invalid request")
+ }
