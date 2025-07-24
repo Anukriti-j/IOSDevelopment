361d3d64ca0345f4c0feed574c1ce37899baa587
Anukriti-j <anu200307@gmail.com>
2025-07-24 22:20:30 +0530

    suggested changes made

--- Week02/Day02/GreetingApp/ViewController.swift
+++ Week02/Day02/GreetingApp/ViewController.swift
@@ -1,25 +1,12 @@
-//
-//  ViewController.swift
-//  MyFirstProject
-//
-//  Created by Coditas on 21/07/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    
     @IBOutlet weak var greetingMsg: UILabel?
     @IBOutlet weak var primaryButton: UIButton?
-    
     @IBOutlet weak var inputName: UITextField?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        // Do any additional setup after loading the view.
-       
     }
     private func setupUI(){
         greetingMsg?.textAlignment = .center
@@ -27,23 +14,15 @@ class ViewController: UIViewController {
         primaryButton?.setTitle("Show Greeting", for: .normal)
         //uicontrol state means the current status or appearance of control: .normal, .selected, .highlighted, .disabled
     }
-
-    func greetingLabel(_ name: String){
+    func updateGreeting(_ name: String){
         greetingMsg?.text = "Hello \(name)!"
     }
-    
-    
     @IBAction func primaryButtonTrigger(_ sender: Any) {
-        if let name = inputName?.text, !name.isEmpty{
-            greetingLabel(name)
-            view.backgroundColor = .white
-        }else{
+        guard let name = inputName?.text, !name.isEmpty else{
             greetingMsg?.text = "please enter your name"
+            return
         }
-        
+        updateGreeting(name)
+        view.backgroundColor = .white
     }
-    
-    
-
 }
-
