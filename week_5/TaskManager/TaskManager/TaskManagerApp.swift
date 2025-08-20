5c7767032bc5ceaae8dca15174bfba0e299308ce
Anukriti-j <anu200307@gmail.com>
2025-08-20 10:34:11 +0530

    Task Manager added

---
+++ week_5/TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                AddTaskView()
+            }
+        }
+    }
+}
